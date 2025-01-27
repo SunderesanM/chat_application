45b4788d2e3df06f8b7022e09723559c4afddd1c
SunderesanM <165923562+SunderesanM@users.noreply.github.com>
2025-01-27 15:00:28 +0530

    check

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,12 @@ import {app,server} from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT
+const __dirname=path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.options("*", cors());
 
 app.use(
   cors({
@@ -26,11 +29,21 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
   })
 );
-app.options("*", cors());  
 
+app.get("/api/hello",(req,res)=>{
+  res.send("hello");
+})
+  
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 server.listen(PORT, ()=>{
     console.log("server is running on port "+PORT);
