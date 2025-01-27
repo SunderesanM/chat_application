c45cd8ec6df5f91562d08a3b234e901919b93ca9
SunderesanM <165923562+SunderesanM@users.noreply.github.com>
2025-01-27 09:56:29 +0530

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure this matches the Vercel settings
+  },
+  base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
 })
