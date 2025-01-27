df70a72731011d5c3d055d8bc42d458184999ea9
SunderesanM <165923562+SunderesanM@users.noreply.github.com>
2025-01-27 19:09:51 +0530

    deployment ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
@@ -10,28 +11,28 @@ import {app,server} from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT
+const __dirname=path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
-    origin: [
-      "https://innovix-chat.vercel.app", // Production frontend URL
-      "http://localhost:5173", // Development frontend URL
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
   })
 );
-
-app.get("/api/hello",(req,res)=>{
-  res.send("hello");
-})
   
+
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 server.listen(PORT, ()=>{
     console.log("server is running on port "+PORT);
