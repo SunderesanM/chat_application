d41dde2c2151e5191dcf23fbba7e1fef2c10cd89
SunderesanM <165923562+SunderesanM@users.noreply.github.com>
2025-01-27 14:35:16 +0530

    finish

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-application-sooty-nine.vercel.app/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-application-rosy-sigma.vercel.app/api",
     withCredentials: true,
 });
